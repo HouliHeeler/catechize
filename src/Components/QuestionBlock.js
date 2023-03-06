870504ed71dd9c95ce531533287a7fa654c62882
HouliHeeler <luke_houlihan@hotmail.com>
2023-03-06 11:08:23 -0800

    clean up randomization coding and add CSS to redacted look

--- src/Components/QuestionBlock.js
+++ src/Components/QuestionBlock.js
@@ -2,8 +2,7 @@ import { useEffect, useState } from 'react'
 
 function QuestionBlock({ questions, handleClick, questionNumber }) {
 
-    const randomNumber = Math.ceil(Math.random() * 4)
-
+    //Randomizes placement of Redacted answer
     const [redacted, setRedacted] = useState(false)
 
     let redactedNumber;
@@ -19,22 +18,49 @@ function QuestionBlock({ questions, handleClick, questionNumber }) {
       setRedacted(false)
     }, [questionNumber])
 
+    //Randomizes placement of correct answer
+    const randomNumber = Math.ceil(Math.random() * 4)
+
+    const orderStyle={
+      order: randomNumber === 1 ? 1 :
+             randomNumber === 2 ? 3 :
+             randomNumber === 3 ? 5 : 7
+    }
+
+
     const questionBlock = questions.map((item, index) => {
         return(
           <section className="card--body" key={index}>
             <div className="card--question">{item.question}</div>
             <div className="card--answers">
               <ul>
-                  <li id='Incorrect' style={{order: 2}} onClick={handleClick}>{redactedNumber === 1 ? "[Redacted]" : item.incorrectAnswers[0]}</li>
-                  <li id='Incorrect' style={{order: 4}} onClick={handleClick}>{redactedNumber === 2 ? "[Redacted]" : item.incorrectAnswers[1]}</li>
-                  <li id='Incorrect' style={{order: 6}} onClick={handleClick}>{redactedNumber === 3 ? "[Redacted]" : item.incorrectAnswers[2]}</li>
                   <li 
-                      id='Correct'
-                      onClick={handleClick}
-                      className={randomNumber === 1 ? "One" : 
-                                 randomNumber === 2 ? "Two" : 
-                                 randomNumber === 3 ? "Three" : "Four"}>
-                                      {redactedNumber === 4 ? "[Redacted]" : item.correctAnswer}
+                    id='Incorrect' 
+                    style={{order: 2}}
+                    className={redactedNumber === 1 && 'redacted'} 
+                    onClick={handleClick}>
+                      {redactedNumber === 1 ? "[Redacted]" : item.incorrectAnswers[0]}
+                  </li>
+                  <li 
+                    id='Incorrect' 
+                    style={{order: 4}}
+                    className={redactedNumber === 2 && 'redacted'} 
+                    onClick={handleClick}>
+                      {redactedNumber === 2 ? "[Redacted]" : item.incorrectAnswers[1]}
+                  </li>
+                  <li 
+                    id='Incorrect' 
+                    style={{order: 6}}
+                    className={redactedNumber === 3 && 'redacted'} 
+                    onClick={handleClick}>
+                      {redactedNumber === 3 ? "[Redacted]" : item.incorrectAnswers[2]}
+                  </li>
+                  <li 
+                    id='Correct'
+                    style={orderStyle}
+                    className={redactedNumber === 4 && 'redacted'}
+                    onClick={handleClick}>
+                      {redactedNumber === 4 ? "[Redacted]" : item.correctAnswer}
                   </li>
               </ul>
             </div>
